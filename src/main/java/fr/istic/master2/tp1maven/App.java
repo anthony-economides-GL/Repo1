04c615ea5ce9ebba446f0054a9f0a473b5b886c8
anthony <anthony.economides@gmail.com>
2013-09-20 15:50:27 +0200

    initial commit

---
+++ src/main/java/fr/istic/master2/tp1maven/App.java
@@ -0,0 +1,26 @@
+package fr.istic.master2.tp1maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	private String test;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        testCodeMort();
+    }
+    
+    public static void testCodeMort(){
+    	System.out.println("code");
+    	if (2>3){
+    		System.out.println("");
+    	}
+    }
+    
+   
+}
